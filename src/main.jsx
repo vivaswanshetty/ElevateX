e21eb3101eda334190fcaca44e9e82ee29bcb271
Vivaswan Shetty <vivaswanshetty@Vivaswans-MacBook-Air.local>
2025-12-11 18:53:34 +0530

    Add Vercel SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
+        <SpeedInsights />
     </React.StrictMode>,
 )
